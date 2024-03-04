6c8a292b635a5c99eeb02975700f6e2572beef5d
cottarie <Krittiphattae@gmail.com>
2024-03-04 20:29:34 +0700

    Project1

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db')
+
+dotenv.config({path:'./config/config.env'});
+
+
+connectDB();
+
+const app = express();
+
+//body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+
+const campgrounds = require('./routes/campgrounds');
+
+const users = require('./routes/auth')
+const bookings=require('./routes/bookings');
+
+app.use('/api/v1/campgrounds',campgrounds);
+
+app.use('/api/v1/auth',users);
+
+app.use('/api/v1/bookings',bookings);
+
+const PORT = process.env.PORT || 5000;
+
+const server =  app.listen( PORT, console.log('Server running in ' ,process.env.NODE_ENV, ' mode on port ', PORT));
+
+process.on('unhandledRejection' , (err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //close server & exit process
+    server.close(()=>process.exit(1));
+});
